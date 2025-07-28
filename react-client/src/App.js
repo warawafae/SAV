272ca57e80a596ca12698eb88178a352779c6df8
warawafae <wafaebarara@gmail.com>
2025-07-28 21:54:39 +0100

    respreclamhistorique

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,35 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Responsable from "./pages/Responsable";
 import ReclamationForm from "./pages/ReclamationForm";
 
-function Home() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* Redirige la racine vers /responsable */}
+        <Route path="/" element={<Responsable />} />
         <Route path="/responsable" element={<Responsable />} />
         <Route path="/reclamation-form" element={<ReclamationForm />} />
       </Routes>
